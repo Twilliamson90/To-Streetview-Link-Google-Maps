502acc5575f336b6243ccbc5936ea6c5af81bc1e
Travis Williamson <Twilliamson90@gmail.com>
2014-03-23 19:02:53 -0400

    clean up

---
+++ js/data.js
@@ -0,0 +1,27 @@
+// of course you would get this from somewhere else (Database, API Call)
+var locationData = [
+	{
+		street: 'GM Renaissance Center',
+		cityState: 'Detroit, MI',
+		lat: 42.329819,
+		lng: -83.040325
+	},
+	{
+		street: 'Central Park',
+		cityState: 'New York, NY',
+		lat: 40.777104,
+		lng: -73.972685
+	},
+	{
+		street: 'The Getty',
+		cityState: 'Los Angeles, CA',
+		lat: 34.077911,
+		lng: -118.475020
+	},
+	{
+		street:	'Main St. Bridge',
+		cityState: 'Jacksonville, FL',
+		lat: 30.322452,
+		lng: -81.658618
+	}
+]
